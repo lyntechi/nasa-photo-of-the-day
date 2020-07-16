8018a5154263725a2ddec37d488dfb46f559e292
lyntechi <lynda-santiago@lambdastudents.com>
2020-07-16 01:35:56 -0400

    added styling to text area using styled-components

--- src/components/Apod.jsx
+++ src/components/Apod.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
 
 function Apod(props){
     const{apod}=props;
+
+    const TextArea = styled.div`
+  
+   font-size: 1.2rem;
+    letter-spacing: 2px;
+    background: rgb(20,40,50);
+    color: whitesmoke;
+    `
+  
     
-    return <div className="apodContainer">
+    
+    return < div className="apodContainer">
              
              
              
+             
+            <img src={apod.hdurl} alt=""/>
             
-             <img src={apod.hdurl} alt=""/>
-             <div className="textSection"><h1>{apod.title}</h1>
-             <p>Copyright: {apod.copyright}</p>
-             <p>Date: {apod.date}</p>
-             <p>{apod.explanation}</p></div>
+            <TextArea><h1>{apod.title}</h1>
+            <p>Copyright: {apod.copyright}</p>
+            <p>Date: {apod.date}</p>
+            <p>{apod.explanation}</p></TextArea>
 
           </div>
 }
